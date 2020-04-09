d915446e4d7735bb3a0d63f3564460328577e4b9
jhamPac <cloud@kevia.me>
2020-04-09 15:52:54 -0700

    moving code to their own packags

--- main.go
+++ main.go
@@ -1,62 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
 
-	"golang.org/x/net/html"
+	"github.com/jhampac/redbull/parsers"
 )
-
-func parseCountryHTML(h []byte) map[string]string {
-	m := make(map[string]string)
-	r := bytes.NewReader(h)
-	tokenizer := html.NewTokenizer(r)
-	done := false
-	for !done {
-		tt := tokenizer.Next()
-		switch tt {
-		case html.ErrorToken:
-			done = true
-		case html.StartTagToken:
-			t := tokenizer.Token()
-			if t.Data == "span" {
-				for _, a := range t.Attr {
-					var key string
-					var value string
-					switch a.Val {
-					case "country-name":
-						tt := tokenizer.Next()
-						if tt == html.TextToken {
-							s := strings.Trim(strings.ToLower(string(tokenizer.Text())), "")
-							rs := strings.ReplaceAll(s, " ", "-")
-							key = rs
-							endtt := tokenizer.Next()
-							if endtt == html.EndTagToken {
-								tt := tokenizer.Next()
-								if tt == html.StartTagToken {
-									t := tokenizer.Token()
-									if t.Data == "span" {
-										tt := tokenizer.Next()
-										if tt == html.TextToken {
-											code := strings.Trim(string(tokenizer.Text()), "")
-											value = code
-										}
-									}
-
-								}
-							}
-							m[key] = value
-						}
-					}
-				}
-			}
-		}
-	}
-	return m
-}
 
 func main() {
 	data, err := ioutil.ReadFile("index.html")
@@ -64,7 +14,7 @@ func main() {
 		log.Fatalf("could not parse the file %v", err)
 	}
 
-	m := parseCountryHTML(data)
+	m := parsers.CountryHTML(data)
 
 	fmt.Printf("All done!\n+%v", m)
 }
---
+++ parsers/parser.go
@@ -0,0 +1,57 @@
+package parsers
+
+import (
+	"bytes"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func CountryHTML(h []byte) map[string]string {
+	m := make(map[string]string)
+	r := bytes.NewReader(h)
+	tokenizer := html.NewTokenizer(r)
+
+	done := false
+	for !done {
+		tt := tokenizer.Next()
+		switch tt {
+		case html.ErrorToken:
+			done = true
+		case html.StartTagToken:
+			t := tokenizer.Token()
+			if t.Data == "span" {
+				for _, a := range t.Attr {
+					var key string
+					var value string
+					switch a.Val {
+					case "country-name":
+						tt := tokenizer.Next()
+						if tt == html.TextToken {
+							s := strings.Trim(strings.ToLower(string(tokenizer.Text())), "")
+							rs := strings.ReplaceAll(s, " ", "-")
+							key = rs
+							endtt := tokenizer.Next()
+							if endtt == html.EndTagToken {
+								tt := tokenizer.Next()
+								if tt == html.StartTagToken {
+									t := tokenizer.Token()
+									if t.Data == "span" {
+										tt := tokenizer.Next()
+										if tt == html.TextToken {
+											code := strings.Trim(string(tokenizer.Text()), "")
+											value = code
+										}
+									}
+
+								}
+							}
+							m[key] = value
+						}
+					}
+				}
+			}
+		}
+	}
+	return m
+}
