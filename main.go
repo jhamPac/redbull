d4766c25f67c560f340542edba544f7bd3d8e345
jhamPac <cloud@kevia.me>
2020-04-09 14:01:00 -0700

    making good progress

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -19,10 +20,28 @@ func main() {
 
 	for {
 		tt := tokenizer.Next()
-		if tt == html.ErrorToken {
+		switch tt {
+		case html.ErrorToken:
 			return
+		case html.StartTagToken:
+			t := tokenizer.Token()
+			if t.Data == "span" {
+				for _, a := range t.Attr {
+					switch a.Val {
+					case "country-name":
+						text := tokenizer.Next()
+						if text == html.TextToken {
+							s := string(tokenizer.Text())
+							fmt.Printf("%s\n", strings.ToLower(s))
+						}
+					case "dial-code":
+						text := tokenizer.Next()
+						if text == html.TextToken {
+							fmt.Printf("%s\n", tokenizer.Text())
+						}
+					}
+				}
+			}
 		}
-		x := tokenizer.Token().Data
-		fmt.Printf("token is: %v", x)
 	}
 }
