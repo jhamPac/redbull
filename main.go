2bd36d17e11d8d2cf5c1c3f80b47422614e2bd91
jhamPac <cloud@kevia.me>
2020-04-09 14:52:52 -0700

    got it saved in a map

--- main.go
+++ main.go
@@ -19,23 +19,26 @@ func main() {
 	r := bytes.NewReader(data)
 	tokenizer := html.NewTokenizer(r)
 
-	// m := make(map[string]string)
+	m := make(map[string]string)
 	for {
 		tt := tokenizer.Next()
 		switch tt {
 		case html.ErrorToken:
-			fmt.Println("All done!")
+			fmt.Printf("All done!\n+%v", m)
 			os.Exit(1)
 		case html.StartTagToken:
 			t := tokenizer.Token()
 			if t.Data == "span" {
 				for _, a := range t.Attr {
+					var key string
+					var value string
 					switch a.Val {
 					case "country-name":
 						text := tokenizer.Next()
 						if text == html.TextToken {
-							s := string(tokenizer.Text())
-							fmt.Printf("the country: %s\n", strings.ToLower(s))
+							s := strings.Trim(strings.ToLower(string(tokenizer.Text())), "")
+							rs := strings.ReplaceAll(s, " ", "-")
+							key = rs
 							endtt := tokenizer.Next()
 							if endtt == html.EndTagToken {
 								n := tokenizer.Next()
@@ -43,15 +46,16 @@ func main() {
 									if tokenizer.Token().Data == "span" {
 										text := tokenizer.Next()
 										if text == html.TextToken {
-											fmt.Printf("the next span text: %s\n", tokenizer.Text())
+											code := strings.Trim(string(tokenizer.Text()), "")
+											value = code
 										}
 									}
 
 								}
 							}
+							m[key] = value
 						}
 					}
-					fmt.Println("")
 				}
 			}
 		}
