1689a699f3fcc98d1817d2156af0de163321d14f
jhamPac <cloud@kevia.me>
2020-04-09 13:22:15 -0700

    save point  moving along

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	_, err := ioutil.ReadFile("index.html")
+	data, err := ioutil.ReadFile("index.html")
 	if err != nil {
 		log.Fatalf("could not parse the file %v", err)
+	}
+	r := bytes.NewReader(data)
+	tokenizer := html.NewTokenizer(r)
+
+	for {
+		tt := tokenizer.Next()
+		if tt == html.ErrorToken {
+			return
+		}
+		x := tokenizer.Token().Data
+		fmt.Printf("token is: %v", x)
 	}
 }
